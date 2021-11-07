7e6bb95ecb15171fd55b43018135233cabcdeed6
Soumen <dassoumen2488@gmail.com>
2021-11-07 10:57:17 +0530

    Checkout screen started

--- lib/checkout/CheckOutScreenPage.dart
+++ lib/checkout/CheckOutScreenPage.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:homebazaar/cart/SavedProducts.dart';
+import 'package:homebazaar/providers/Providers.dart';
 
 class CheckOutScreenPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+    final savedProducts = watch(cartProductsNotifier).savedProductsInCart;
     return Scaffold(
       appBar: AppBar(title: Text("Product Checkout")),
       body: Stack(
@@ -40,7 +43,68 @@ class CheckOutScreenPage extends ConsumerWidget {
                   ),
                 ),
               ),
+              Expanded(
+                flex: 2,
+                child: Card(
+                    child: Padding(
+                      padding:EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                              child:Column(
+                                children: [
+                                  Text('Order id',
+                                    style: TextStyle(
+                                        color: Colors.grey[400],
+                                        fontSize: 14.0
+                                    ),),
+                                  SizedBox(height: 5.0,),
+                                  Text("88",
+                                    style: TextStyle(
+                                      fontSize: 15.0,
+                                    ),)
+                                ],
+                              )
+                          ),
 
+                          Container(
+                            child: Column(
+                                children: [
+                                  Text('Order Date',
+                                    style: TextStyle(
+                                        color: Colors.grey[400],
+                                        fontSize: 14.0
+                                    ),),
+                                  SizedBox(height: 5.0,),
+                                  Text('November 7th',
+                                    style: TextStyle(
+                                      fontSize: 15.0,
+                                    ),)
+                                ]),
+                          ),
+
+                          Container(
+                              child:Column(
+                                children: [
+                                  Text('Delivery By',
+                                    style: TextStyle(
+                                        color: Colors.grey[400],
+                                        fontSize: 14.0
+                                    ),),
+                                  SizedBox(height: 5.0,),
+                                  Text('November 11',
+                                    style: TextStyle(
+                                      fontSize: 15.0,
+                                    ),)
+                                ],
+                              )
+                          ),
+                        ],
+                      ),
+                    )
+                ),
+              ),
               Expanded(
                 flex:5,
                 child: Container(
@@ -48,124 +112,65 @@ class CheckOutScreenPage extends ConsumerWidget {
                   child: Center(
                       child:Card(
                           margin: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
-                          child: Container(
-                              width: 310.0,
-                              height:290.0,
-                              child: Padding(
-                                padding: EdgeInsets.all(10.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Product Purchase Details",
-                                      style: TextStyle(
-                                        fontSize: 17.0,
-                                        fontWeight: FontWeight.w800,
-                                      ),),
-                                    Divider(color: Colors.grey[300],),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        Icon(
-                                          Icons.home,
-                                          color: Colors.blueAccent[400],
-                                          size: 35,
-                                        ),
-                                        SizedBox(width: 20.0,),
-                                        Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text("Guild",
-                                              style: TextStyle(
-                                                fontSize: 15.0,
-                                              ),),
-                                            Text("FairyTail, Magnolia",
-                                              style: TextStyle(
-                                                fontSize: 12.0,
-                                                color: Colors.grey[400],
-                                              ),)
-                                          ],
-                                        )
-
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              )
-                          )
+                          child: Consumer(
+                              builder: (builder, watch , child){
+                                return Container(
+                                  child :_buildCheckOutList(savedProducts),
+                                );
+                              }
+                          ),
                       )
                   ),
                 ),
               ),
-
             ],
           ),
-          Positioned(
-              top:MediaQuery.of(context).size.height*0.45,
-              left: 20.0,
-              right: 20.0,
-              child: Card(
-                  child: Padding(
-                    padding:EdgeInsets.all(16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                            child:Column(
-                              children: [
-                                Text('Order id',
-                                  style: TextStyle(
-                                      color: Colors.grey[400],
-                                      fontSize: 14.0
-                                  ),),
-                                SizedBox(height: 5.0,),
-                                Text("88",
-                                  style: TextStyle(
-                                    fontSize: 15.0,
-                                  ),)
-                              ],
-                            )
-                        ),
 
-                        Container(
-                          child: Column(
-                              children: [
-                                Text('Order Date',
-                                  style: TextStyle(
-                                      color: Colors.grey[400],
-                                      fontSize: 14.0
-                                  ),),
-                                SizedBox(height: 5.0,),
-                                Text('November 7th',
-                                  style: TextStyle(
-                                    fontSize: 15.0,
-                                  ),)
-                              ]),
-                        ),
-
-                        Container(
-                            child:Column(
-                              children: [
-                                Text('Delivery By',
-                                  style: TextStyle(
-                                      color: Colors.grey[400],
-                                      fontSize: 14.0
-                                  ),),
-                                SizedBox(height: 5.0,),
-                                Text('November 11',
-                                  style: TextStyle(
-                                    fontSize: 15.0,
-                                  ),)
-                              ],
-                            )
-                        ),
-                      ],
-                    ),
-                  )
-              )
-          )
         ],
 
       ),
+    );
+  }
+
+  ListView _buildCheckOutList(List<SavedProducts> productList){
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: productList.length,
+        itemBuilder: (context, index){
+            SavedProducts savedProducts = productList[index];
+            return ListTile(
+              title: Text(savedProducts.product.title!),
+            );
+        }
+    );
+  }
+
+  Widget getRow(){
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Icon(
+          Icons.home,
+          color: Colors.blueAccent[400],
+          size: 35,
+        ),
+        SizedBox(width: 20.0,),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Guild",
+              style: TextStyle(
+                fontSize: 15.0,
+              ),),
+            Text("FairyTail, Magnolia",
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.grey[400],
+              ),)
+          ],
+        )
+
+      ],
     );
   }
 }
